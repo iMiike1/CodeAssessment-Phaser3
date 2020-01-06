909af55334a5c1ec09c0abd441a9be24bf35e00a
Michael <parolinimichael@gmail.com>
2020-01-06 03:02:49 +0000

    Started Implementation of bushes

--- phaser3-project-template-master/src/index.js
+++ phaser3-project-template-master/src/index.js
@@ -8,7 +8,7 @@ import bushImg from "./assets/bush.png"
 var config = {
   type: Phaser.AUTO,
     width: 760,
-  height: 950,
+  height: 260,
   parent : 'phaser-example',
   physics: {
     default: 'arcade',
@@ -27,10 +27,6 @@ var config = {
   }
 };
 
-var stone = null;
-
-var platform;
-
 
 // eslint-disable-next-line no-unused-vars
 var game = new Phaser.Game(config);
@@ -45,33 +41,42 @@ function preload() {
 }
 
 
+var stone = null;
+var platform = null;
+var jumpButton = null;
+var cursors = null;
 
-var cursors;
-var jumpButton;
+var BushesA = null;
+
+
 function create() {
 
 var offset = 380;
 
-//this.physics.world.defaults.debugShowBody = true;
-//this.physics.world.defaults.bodyDebugColor = 0xff00ff;
+
    var bg = this.add.image(760 +offset, 130, "background");
    var bg2 = this.add.image(0 +offset, 130, "background");
 
-   platform = this.physics.add.staticImage(380,260, 'platform');
+   
+  BushesA = this.add.group();
+  for (var i =0; i < 5; i ++ )
+  {
+    BushesA.crea
 
+  }
+
+
+
+   platform = this.physics.add.staticImage(380,260, 'platform');
    stone = this.physics.add.image(400, 150, 'stone');
    stone.body.allowGravity = true;
-   //stone.body.gravity = 1;
-   //stone.setOrigin(0.5, 0);
-   //stone.body.gravity = 0.1;
-   //stone.setVelocity(-380, 0);
    stone.setBounce(0.2);
    stone.setCollideWorldBounds(true);
 
    var grass = this.add.image(760 +offset, 237, 'grass');
    var grass2 = this.add.image(0  +offset, 237, 'grass');
     
-  
+   
   this.tweens.add({
     targets: grass, 
      x: 380,
@@ -102,25 +107,9 @@ var offset = 380;
     loop: -1
       
     });
-
-    this.tweens.add({
-
-      targets: bush1,
-      x:-50,
-      duration: 15000,
-      loop: -1
-
-    });
-
-    var bush1 = this.add.group({key: 'bush', frame:0, repeat:5, setXY:{x:32, y: 50, stepX: 150}});
     
 
-    //Phaser.Actions.IncX(bush1.getChildren(),100);
-    //Phaser.Actions.setVelocity();
-
-    //this.sys.events.on('postupdate', update, this);
-
-    cursors = this.input.keyboard.createCursorKeys();
+    //cursors = this.input.keyboard.createCursorKeys();
     jumpButton = this.input.keyboard.addKey('SPACE');
 
     this.physics.add.collider(stone, platform);
@@ -129,9 +118,6 @@ var offset = 380;
 
  function update(){
 
-  
-
-
   if (jumpButton.isDown){
     
     
@@ -139,14 +125,7 @@ var offset = 380;
   console.log( stone.y);
     
   }
-  if (jumpButton.isUp)
-  {
-    
-    
-    //stone.body.gravity = 1;
-
-  }
-//console.log('diocan');
+ 
 
  }
 
