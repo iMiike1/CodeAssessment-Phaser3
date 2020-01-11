9c12df893557a08c9d52cc659ff9d45d17d0aa7b
Michael <parolinimichael@gmail.com>
2020-01-11 02:36:59 +0000

    AnimationsImplemented

--- phaser3-project-template-master/src/index.js
+++ phaser3-project-template-master/src/index.js
@@ -64,6 +64,7 @@ function preload() {
   this.load.image('cherry', cherryImg);
   this.load.image('stars', starImg);
   this.load.image('shield', shieldImg);
+  this.load.spritesheet('run', dudeImg, {frameWidth :32, frameHeight: 48});
 
 }
 
@@ -120,6 +121,16 @@ var percentage = 1;
 //Create Function
 function create() {
 
+
+var config = {
+  key: 'running',
+  frames : this.anims.generateFrameNumbers('run', {frames: [5,6,7,8]}),
+  frameRate:10,
+  repeat: -1
+};
+
+
+
 var offset = 380;
 
 this.input.enabled = true;
@@ -159,6 +170,9 @@ this.input.enabled = true;
   EnemyBee.body.allowGravity = false;
   EnemyBee.setVelocity(-80,-10);
 
+  this.anims.create(config);
+  //this.add.sprite(150,150, 'run').play('running');
+
 
    Bushes.add(Bush1);
    Bushes.add(Bush2);
@@ -188,20 +202,20 @@ this.input.enabled = true;
 
    platform = this.physics.add.staticImage(380,260, 'platform');
 
-   player = this.physics.add.image(350, 150,'player');
+   player = this.physics.add.sprite(300,150, 'run').play('running');
    player.body.allowGravity = true;
    player.setBounce(0.2);
    player.setCollideWorldBounds(true);
    player.setInteractive();
 
-   player1 = this.physics.add.image(player.x - 80, 150, 'player');
+   player1 = this.physics.add.sprite(player.x - 80, 150, 'run').play('running');
    player1.body.allowGravity = true;
    player1.setBounce(0.2);
    player1.setCollideWorldBounds(true);
    player1.setInteractive();
 
 
-   player2 = this.physics.add.image(player.x + 80, 150, 'player');
+   player2 = this.physics.add.sprite(player.x + 80, 150, 'run').play('running');
    player2.body.allowGravity = true;
    player2.setBounce(0.2);
    player2.setCollideWorldBounds(true);
